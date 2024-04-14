3f88e8ec9ba2412f25e70838002663ab1e682aae
julian <julianprino@gmail.com>
2024-04-14 19:53:17 -0300

    test(go-poc): add test to middlewares

---
+++ cmd/web/middlewere/logger_test.go
@@ -0,0 +1,81 @@
+package middlewere
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"net/http"
+	"testing"
+)
+
+func TestLogger_ServeHTTP(t *testing.T) {
+	tests := []struct {
+		name        string
+		method      string
+		url         string
+		expectedLog string
+	}{
+		{
+			name:        "GET request",
+			method:      "GET",
+			url:         "/test",
+			expectedLog: "GET /test",
+		},
+		{
+			name:        "POST request",
+			method:      "POST",
+			url:         "/another",
+			expectedLog: "POST /another",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			var buf bytes.Buffer
+			log.SetOutput(&buf)
+
+			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+			logger := NewLogger(handler)
+
+			req, err := http.NewRequest(tt.method, tt.url, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			logger.ServeHTTP(nil, req)
+
+			expectedLog := tt.expectedLog
+			assert.Contains(t, buf.String(), expectedLog, "handler did not log the expected message")
+		})
+	}
+}
+
+func TestNewLogger(t *testing.T) {
+	handlerToWrap := http.NewServeMux()
+	type args struct {
+		handlerToWrap http.Handler
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Logger
+	}{
+		{
+			name: "Should create a new logger instance",
+			args: args{
+				handlerToWrap,
+			},
+			want: &Logger{
+				handlerToWrap,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewLogger(tt.args.handlerToWrap)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
---
+++ cmd/web/middlewere/response_header_test.go
@@ -0,0 +1,80 @@
+package middlewere
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNewResponseHeader(t *testing.T) {
+	handlerToWrap := http.NewServeMux()
+
+	type args struct {
+		handlerToWrap http.Handler
+		headerName    string
+		headerValue   string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ResponseHeader
+	}{
+		{
+			name: "Should create a new response header",
+			args: args{
+				handlerToWrap: handlerToWrap,
+				headerName:    "Content-Type",
+				headerValue:   "application/json",
+			},
+			want: &ResponseHeader{
+				handler:     handlerToWrap,
+				headerName:  "Content-Type",
+				headerValue: "application/json",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewResponseHeader(tt.args.handlerToWrap, tt.args.headerName, tt.args.headerValue)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestResponseHeader_ServeHTTP(t *testing.T) {
+	tests := []struct {
+		name          string
+		headerName    string
+		headerValue   string
+		expectedValue string
+	}{
+		{
+			name:          "Should add header to response",
+			headerName:    "Content-Type",
+			headerValue:   "application/json",
+			expectedValue: "application/json",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+			responseHeader := NewResponseHeader(handler, tt.headerName, tt.headerValue)
+
+			req, err := http.NewRequest("GET", "/", nil)
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rec := httptest.NewRecorder()
+
+			responseHeader.ServeHTTP(rec, req)
+
+			assert.Equal(t, tt.expectedValue, rec.Header().Get(tt.headerName))
+		})
+	}
+}

a1263823d39514557f67e05ac72cd5ea309e04b7
julian <julianprino@gmail.com>
2024-04-14 21:20:13 -0300

    doc(go-poc): add docker and ddl for oracle db

---
+++ docker/scripts/create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE pokedex (
+                         id NUMBER PRIMARY KEY,
+                         pokemon_id NUMBER,
+                         type VARCHAR2(50),
+                         alias VARCHAR2(100)
+);
